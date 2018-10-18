e9233b2b321eef9769375a859cf469a3d31dad34
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-18 21:52:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_min_value = ""
-  array_values = name_hash.to_a
+  if name_hash == {}
+    nil
+  else
+  
   name_hash.collect do |key, value|
     
 
